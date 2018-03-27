fe1e389106cb6fdb52e6732aa03a29eb535701c7
Susan Post <epost@uccs.edu>
2018-03-26 20:00:45 -0600

    Train and Test splits made, confusion matricies plotted, works for data of size 100

--- img_extraction1.py
+++ img_extraction1.py
@@ -1,4 +1,3 @@
-print("Running")
 
 from scipy import misc
 from sklearn.decomposition import IncrementalPCA
@@ -9,48 +8,172 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn import ensemble
 import numpy as np
 from sklearn.preprocessing import OneHotEncoder
+from PIL import Image
+import copy
+import matplotlib.pyplot as plt
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import confusion_matrix
 
-#this should reduce the dimensionality of the data set, but not sure we need to
-#img_data = misc.imread('ISIC_0000000.jpg')
-#print(img_data.shape)
-#ipcs = IncrementalPCA(n_components=2, batch_size=10)
-#img_ipca = ipcs.partial_fit(img_data)
+#######################################################################################################################################
+#	WORKS ONLY FOR A DATA SET OF SIZE 100
+
+#######################################################################################################################################
+
+#code to plot confusion matrix
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+#end of plot confusion matrix
+#######################################################################################################################################
 
 
-
-#extract whether benign or malignant from files
+#######################################################################################################################################
+#create target array with data from files
 target_array = []
-for filename in os.listdir(): #for every json (has text) file in our data set
-	if filename.endswith("json"):
-		data = json.load(open(filename))
+for filename in os.listdir(): #extract whether benign or malignant from files
+	if filename.endswith("json"): #for every json (has text) file in our data set
+		data = json.load(open(filename)) #load
 		target_array.append(data["meta"]["clinical"]["benign_malignant"]) #extract whether it is benign or malignant from file
 
 target_array = np.asarray(target_array)#make numpy array
 
+#transform benign to 0 and malignant to 1
 labelencoder = LabelEncoder()
-target_array = labelencoder.fit_transform(target_array) #transform benign to 0 and malignant to 1
-#fake_array = np.array([1] * 557) #when passed in instead of data array, this works, so we want data array to resemble this somehow
-#fake_array = fake_array.reshape(-1,1)
+target_array = labelencoder.fit_transform(target_array) 
+
+#######################################################################################################################################
 
 
-enc = OneHotEncoder(categorical_features = [0]) #added in hopes of fixing error, no change yet
-target_array = target_array.reshape(-1,1)
-target_array = enc.fit_transform(target_array).toarray()
-
+#######################################################################################################################################
+#create data array with data from pictures only
 data_array = []
-
 for filename in os.listdir(): #transform images into matrix of numbers and store in data array
-	if filename.endswith("jpg"):
-		data_array.append(misc.imread(filename))
+	if filename.endswith("jpg"): #for all jpg (images) in our file
+		data_array.append(misc.imread(filename, mode = 'F')) #open and read by pixel, putting information into 3d array: 1d - all pictures, 2d - individual picture, 3d - individual pixel
+
+		#crop each image and resave so that all 2d - arrays are the same size
+		basewidth = 300 
+		img = Image.open(filename)
+		wpercent = (basewidth/float(img.size[0]))
+		hsize = int((float(img.size[1])*float(wpercent)))
+		img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+		img.save(filename) 
 
 data_array = np.asarray(data_array) #make data array numpy array
-print(type(target_array))
-print(type(data_array))
-data_array = data_array.reshape(557,1)
+#######################################################################################################################################
 
-#target_array = target_array.reshape(557,2)
-clf = ensemble.RandomForestClassifier(n_jobs=2, random_state=0) #create decision tree
-clf.fit(data_array, target_array)
+
+#######################################################################################################################################
+
+
+
+
+x_train, x_test, y_train, y_test = train_test_split(data_array, target_array, test_size = 0.36, random_state = 0) #split composite data into test and train sets
+
+nsamples, nx, ny = x_train.shape #get length of dimensions in order to be able to properly reshape
+msamples, mx, my = x_test.shape
+
+'''
+#for debugging with new datasets
+print("nsamples:")
+print(nsamples)
+
+print("nx:")
+print(nx)
+print("ny:")
+print(ny)
+
+print("msamples:")
+print(msamples)
+
+print("mx:")
+print(mx)
+print("my:")
+print(my)
+'''
+
+x_train = x_train.reshape(nsamples,nx*ny) #reshape training (data) array with dimensions obtained above for Classifier
+x_test = x_test.reshape(msamples, mx*my) #reshape test (data) array with dimensions obtained above for Classifier
+
+clf = ensemble.RandomForestClassifier(n_estimators = 100, random_state=0) #estimators = # of nodes, set random_state prevents it from changing inbetween runs
+tree = clf.fit(x_train, y_train) #create tree from training data
+
+
+
+#######################################################################################################################################
+
+
+#######################################################################################################################################
+#test tree and create measures of fit
+y_prediction = clf.predict(x_test) #test how well built tree predicts unseen images
+#print(y_prediction) #returns an array of 0 or 1 for benign or malignant
+
+class_names = "benign", "malignant" #needed to plot confusion matrix
+cnf_matrix = confusion_matrix(y_test, y_prediction) #create confusion matrix from test data
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, title='Confusion matrix, without normalization')
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True, title='Normalized confusion matrix')
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
